e93e27c6fb84ec80efc2df76bf054ed12b110ae0
Yakup Bozkurt <bozkur.yakup.2000@gmail.com>
2023-04-09 02:21:20 +0300

    Initial commit

---
+++ src/main/java/codinigio/musicio/MusicioApplication.java
@@ -0,0 +1,14 @@
+package codinigio.musicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicioApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MusicioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codinigio/musicio/MusicioApplicationTests.java
@@ -0,0 +1,13 @@
+package codinigio.musicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
